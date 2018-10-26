762a86b691d10fa37c6d70b2821d0c4807c2aac4
Darshan Baid <darshanbaid22@gmail.com>
2018-10-27 02:40:21 +0530

    q3, q4

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,17 +10,19 @@ const app = dialogflow({
 });
 i18n.use(app);
 
-const CANT_SAY  = -1;
+const CANT_SAY  = "-1";
 const questions = {
     "q1": {
         "question-type": "image",
         "image-source": "https://www.colour-blindness.com/CBTests/ishihara/Plate1.gif",
+        "plate-code": 1,
         "choices": [12,24,CANT_SAY],
         "scores": {
             "12": {
                 "creepy": 0,
                 "red-green": 0,
-                "total": 0
+                "total": 0,
+                "normal": 0
             },
             "default" : {
                 "creepy": 0.5,
@@ -29,22 +31,70 @@ const questions = {
     },
     "q2": {
         "question-type": "image",
-        "image-source": "https://www.colour-blindness.com/CBTests/ishihara/Plate1.gif",
-        "choices": [12,24,CANT_SAY],
-        "correct_answer": 12
+        "image-source": "https://www.colour-blindness.com/CBTests/ishihara/Plate2.gif",
+        "plate-code": 2,
+        "choices": [8,3,CANT_SAY],
+        "scores": {
+            "8": {
+                "normal": 0.2
+            },
+            "3": {
+                "red-green": 0.2,
+                "total": 0.1,
+                "normal": -0.1
+            },
+            CANT_SAY: {
+                "total": 0.2,
+                "normal": -0.1
+            },
+            "default" : {
+                "creepy": 0.1,
+            }
+        }
     },
     "q3": {
         "question-type": "image",
-        "image-source": "https://www.colour-blindness.com/CBTests/ishihara/Plate1.gif",
-        "choices": [12,24,CANT_SAY],
-        "correct_answer": 12
+        "image-source": "https://www.colour-blindness.com/CBTests/ishihara/Plate8.gif",
+        "plate-code": 8,
+        "choices": [6,9,3,CANT_SAY],
+        "scores": {
+            "6": {
+                "normal": 0.2
+            },
+            CANT_SAY: {
+                "total": 0.2,
+                "red-green": 0.2,
+                "normal": -0.1
+            },
+            "default" : {
+                "total": 0.1,
+                "red-green": 0.1
+            }
+        }
     },
     "q4": {
         "question-type": "image",
-        "image-source": "https://www.colour-blindness.com/CBTests/ishihara/Plate1.gif",
-        "choices": [12,24,CANT_SAY],
-        "correct_answer": 12
-    }
+        "image-source": "https://www.colour-blindness.com/CBTests/ishihara/Plate2.gif",
+        "plate-code": 2,
+        "choices": [8,3,CANT_SAY],
+        "scores": {
+            "8": {
+                "normal": 0.2
+            },
+            "3": {
+                "red-green": 0.2,
+                "total": 0.1,
+                "normal": -0.1
+            },
+            CANT_SAY: {
+                "total": 0.2,
+                "normal": -0.1
+            },
+            "default" : {
+                "creepy": 0.1,
+            }
+        }
+    },
 }
 
 function randomChoice(s: string,conv, ex?: object) {
@@ -55,12 +105,12 @@ function randomChoice(s: string,conv, ex?: object) {
 }
 
 function askImage(q: string, conv) {
-    conv.ask(randomChoice("WHATS_THIS_IMAGE", conv),new BasicCard({
+    conv.ask(new BasicCard({
         image: new Image({
             url: questions[q]['image-source'],
             alt: conv.__('img-alt'),          
         }),
-    }));
+    }), new Suggestions(questions[q]['choices']));
 }
 function askQuestion(conv) {
     // First check if it is the first question
@@ -75,6 +125,7 @@ function askQuestion(conv) {
             "total": 0
         };
         conv.user.storage['quiz']['total_questions'] = 1;
+        conv.ask(randomChoice("WHATS_THIS_IMAGE", conv));
         askImage('q1',conv);
         
     }
@@ -82,8 +133,8 @@ function askQuestion(conv) {
 
 function tellAnswer(conv, answer?) {
     const q = questions[conv.user.storage['quiz']['last_question']];
+    let scores = {};
     if (q['question-type'] === 'image') {
-        let scores = {};
         if (q['scores'][`${answer}`] !== undefined) {
             // such an answer exist
             scores = q['scores'][`${answer}`];
@@ -101,9 +152,25 @@ function tellAnswer(conv, answer?) {
         conv.close(randomChoice("JOKE_ANYONE",conv),randomChoice("JOKE_BYE",conv));
         return;
     } else if (conv.user.storage['quiz']['scores']['red-green'] > 0.7) {
+        // The user probably has red-green color blindness
         conv.close(randomChoice("YOU_HAVE_RED_GREEN", conv));
     } else if (conv.user.storage['quiz']['scores']['total'] > 0.7) {
+        // The user probably has total color blindnesss
         conv.close(randomChoice("YOU_HAVE_TOTAL", conv));
+    } else if (conv.user.storage['quiz']['red-green'] > conv.user.storage['quiz']['total']) {
+        // Probablity of having red-green, so we'll ask a more specific question
+        conv.contexts.set('image_question',5);
+        conv.ask(randomChoice("WHATS_THIS_IMAGE", conv));
+        askImage('q2',conv);
+    } else if (conv.user.storage['quiz']['red-green'] > 0.3) {
+        conv.contexts.set('image_question',5);
+        conv.ask(randomChoice("WHATS_THIS_IMAGE", conv));
+        askImage('q4',conv);
+    } else {
+        conv.contexts.set('image_question', 5);
+        conv.ask(randomChoice("WHATS_THIS_IMAGE", conv));
+        askImage('q4',conv);
+
     }
 }
 
@@ -140,5 +207,6 @@ app.intent("Start Question", (conv) => {
 
 app.intent("Image Question", (conv) => {
     const answer = +conv.parameters["number"];
+    tellAnswer(conv, answer);
 })
 exports.googleAssistantAction = functions.https.onRequest(app);
